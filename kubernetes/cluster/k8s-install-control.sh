0dfc519b20becf95f649f357d4f93abe97f5dea1
cmayen <cmayen@gmail.com>
2025-07-25 21:33:55 -0500

    kube ubuntu metal installers

---
+++ kubernetes/cluster/k8s-install-control.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# Replace <your_pod_network_cidr> with a suitable CIDR,
+# e.g., 10.244.0.0/16 for Flannel
+sudo kubeadm init --pod-network-cidr=10.224.0.0/16
+
+#
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# apply flannel yaml from master project at github
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+#kubectl apply -f flannel.yaml
+
+# flannel for some reason doesnt take the network correctly
+# from the yaml?? wtheck man
+kubectl -n kube-flannel patch configmap kube-flannel-cfg \
+--type merge \
+-p '{"data":{"net-conf.json":"{\n \"Network\": \"10.224.0.0/16\",\n \"Backend\": {\"Type\":\"vxlan\"}\n}\n"}}'
+
+
+# whoaaa. slow down a sec or 2
+sleep 3
+
+
+# To allow pods to be scheduled on the control plane
+# node, remove this taint using the kubectl taint command:
+# (not recommended) this is here only for development tests
+#kubectl taint nodes fusion node-role.kubernetes.io/control-plane:NoSchedule-
+
+
+# output stuffs
+echo "kubectl get nodes && kubectl get pods -A"
---
+++ kubernetes/cluster/k8s-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# run initial updates
+sudo apt update
+sudo apr upgrade -y
+
+# disable swap
+sudo swapoff -a
+sudo sed -i '/\/swap\.img/ s/^/#/' /etc/fstab
+
+# kernel stuff
+sudo modprobe overlay
+sudo modprobe br_netfilter
+# persist after reboot
+echo overlay | sudo tee /etc/modules-load.d/overlay.conf
+echo br_netfilter | sudo tee -a /etc/modules-load.d/overlay.conf
+
+# 
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# install containerd
+sudo apt update
+sudo apt install -y containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# setup to add kubernetes apt repo
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# install k8s
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# mark as hold
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+
