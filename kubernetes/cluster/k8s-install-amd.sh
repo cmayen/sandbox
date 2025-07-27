ceae1005fb8bc9ab30ea9bba96475c002411b534
cmayen <cmayen@gmail.com>
2025-07-27 10:04:59 -0500

    add ollama-rocm yaml

--- kubernetes/cluster/k8s-install-amd.sh
+++ kubernetes/cluster/k8s-install-amd.sh
@@ -5,7 +5,7 @@ sudo apt install -y ./amdgpu-install_6.3.60304-1_all.deb
 sudo amdgpu-install --usecase=dkms
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amd-container-toolkit/apt/ jammy main" | sudo tee /etc/apt/sources.list.d/amd-container-toolkit.list
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install amd-container-toolkit
+sudo apt install amd-container-toolkit rocm-smi btop
 sudo amd-ctk runtime configure
 sudo apt install -y docker.io docker-compose-v2
 sudo amd-ctk runtime configure
